5ec5f22f2adaa7ef64c413e1ef022a025f6a4f70
Josiah Green <JosiahGreen90@gmail.com>
2018-12-17 13:32:18 -0800

    solution code passing all tests

--- exercises/fizzbuzz/fizzbuzz.js
+++ exercises/fizzbuzz/fizzbuzz.js
@@ -13,6 +13,22 @@
 //   buzz
 
 export default function fizzBuzz(n) {
+  let counter = 0;
 
+  while (counter < n) {
+    counter += 1;
+
+    if (counter % 3 === 0 && counter % 5 === 0) {
+      console.log('fizzbuzz');
+    } else if (counter % 5 === 0) {
+      console.log('buzz');
+    } else if (counter % 3 === 0) {
+      console.log('fizz');
+    } else {
+      console.log(counter);
+    }
+  }
+
+  return undefined;
 }
 
