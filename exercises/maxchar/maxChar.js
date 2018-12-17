3d68fff2c4789a07190c7a78d63ac22bb1dff5ef
Josiah Green <JosiahGreen90@gmail.com>
2018-12-17 13:11:05 -0800

    solution passing

--- exercises/maxchar/maxChar.js
+++ exercises/maxchar/maxChar.js
@@ -6,6 +6,24 @@
 // maxChar("apple 1231111") === "1"
 
 export default function maxChar(str) {
-  
+  const charMap = {};
+  let max = 0;
+  let maxChars = '';
+
+  for (let char of str) { // eslint-disable-line
+    if (charMap[char]) {
+      charMap[char] += 1;
+    } else {
+      charMap[char] = 1;
+    }
+  }
+
+  for (let char in charMap) { // eslint-disable-line
+    if (charMap[char] > max) {
+      max = charMap[char];
+      maxChars = char;
+    }
+  }
+  return maxChars;
 }
 
