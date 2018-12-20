e7cd4637a3fd7ba37e1cb61e2f2216ee5ab50f6a
Josiah Green <JosiahGreen90@gmail.com>
2018-12-19 16:57:58 -0800

    tests pass

--- exercises/chunk/chunk.js
+++ exercises/chunk/chunk.js
@@ -9,12 +9,12 @@
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
 export default function chunk(array, size) {
-  let chunkCount = 1;
+  let chunkCount = 0;
   const chunkedArr = [];
-  const subArr = [];
+  let subArr = [];
 
-  for (let i = 0; i < array.size - 1; i++) {
-    if (chunkCount <= size) {
+  for (let i = 0; i < array.length; i++) {
+    if (chunkCount < size) {
       subArr.push(array[i]);
       chunkCount += 1;
     }
@@ -22,7 +22,12 @@ export default function chunk(array, size) {
     if (chunkCount === size) {
       chunkCount = 0;
       chunkedArr.push(subArr);
+      subArr = [];
     }
+  }
+
+  if (array.length % size !== 0) {
+    chunkedArr.push(subArr);
   }
   
   return chunkedArr;
